80b3f22c06ce553ae17afaa791ddbd9567429712
Your Name <youremail@example.com>
2023-09-03 16:44:13 +0530

    1.6

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let fetchbtn = document.getElementById("fetch-btn")
  
 fetchbtn.addEventListener("click",()=>{
    
-    window.location.href="http://127.0.0.1:5500/landing.html";
+    document.location="http://127.0.0.1:5500/landing.html";
 
     // window.document.href="https://pavannavde.github.io/weather-app/landing"
     
